84d0ead5cd210543236a1ca3a928cc64efa371d8
Agata <agata.pas@wp.pl>
2022-09-12 17:55:50 +0200

    admin login admin news

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
 var createError = require('http-errors');
+var cookieSession = require('cookie-session');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var config = require('./config');
+
+///połączenie z bazą danych
+var mongoose = require('mongoose')
+
+mongoose.connect(config.db, { useNewUrlParser: true });
+
+
+var db = mongoose.connection;
+db.once('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function () {
+//   console.log('db connect')
+// })
+
+// mongoose.connection.on('error', err => {
+//   logError(err);
+// });
 
 var indexRouter = require('./routes/index');
 var newsRouter = require('./routes/news');
 var quizRouter = require('./routes/quiz');
 var adminRouter = require('./routes/admin');
-
 
 
 var app = express();
@@ -22,6 +39,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieSession({
+  name: 'session',
+  keys: config.keySession,
+
+  // Cookie Options
+  maxAge: config.maxAgeSession // 24 hours
+}))
+
 
 
 app.use(function (req, res, next) {
---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    db: 'mongodb+srv://admin:HnyJMNfYVOCydGmQ@cluster0.dfhel.mongodb.net/?retryWrites=true&w=majority',
+    keySession: ['TWOJKLUCZ'],
+    maxAgeSession: 24 * 60 * 60 * 1000
+}
